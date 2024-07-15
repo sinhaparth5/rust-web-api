aa7ba117ec43e235fb6cf42206af6297e129e063
Parth Sinha <sinhaparth555@gmail.com>
2024-07-15 23:46:33 +0100

    added basic auth function

--- src/main.rs
+++ src/main.rs
@@ -2,42 +2,44 @@
 
 use rocket::http::Status;
 use rocket::Request;
-use rocket::request::{FromRequest, Outcome};
+use rocket::request::{self, FromRequest, Outcome};
 use rocket::response::status;
 use rocket::serde::json::{Value, json};
+use base64::engine::general_purpose;
+use base64::Engine;
 
 struct BasicAuth {
     pub username: String,
     pub password: String,
 }
 
- impl BasicAuth {
-     fn from_authorization_header(header: &str) -> Option<BasicAuth> {
-         let split = header.split_whitespace().collect::<Vec<_>>();
-         if split.len() != 2 {
-             return None;
-         }
+impl BasicAuth {
+    fn from_authorization_header(header: &str) -> Option<BasicAuth> {
+        let split = header.split_whitespace().collect::<Vec<_>>();
+        if split.len() != 2 {
+            return None;
+        }
 
-         if split[0] != "Basic" {
-             return None;
-         }
-         Self::from_base64_encoded(split[1])
-     }
+        if split[0] != "Basic" {
+            return None;
+        }
+        Self::from_base64_encoded(split[1])
+    }
 
-     fn from_base64_encoded(base64_string: &str) -> Option<BasicAuth> {
-         let decoded = base64::decode(base64_string).ok()?;
-         let decoded_str = String::from_utf8(decoded).ok()?;
-         let split = decoded_str.split(":").collect::<Vec<_>>();
-         if split.len() != 2 {
-             return None;
-         }
-         let (username, password) = (split[0].to_string(), split[1].to_string());
-         Some(BasicAuth {
-             username,
-             password
-         })
-     }
- }
+    fn from_base64_encoded(base64_string: &str) -> Option<BasicAuth> {
+        let decoded = base64::decode(base64_string).ok()?;
+        let decoded_str = String::from_utf8(decoded).ok()?;
+        let split = decoded_str.split(":").collect::<Vec<_>>();
+        if split.len() != 2 {
+            return None;
+        }
+        let (username, password) = (split[0].to_string(), split[1].to_string());
+        Some(BasicAuth {
+            username,
+            password
+        })
+    }
+}
 
 #[rocket::async_trait]
 impl<'r> FromRequest<'r> for BasicAuth {
@@ -54,7 +56,7 @@ impl<'r> FromRequest<'r> for BasicAuth {
 }
 
 #[get("/rust-api")]
-fn get_rust_api() -> Value {
+fn get_rust_api(_auth: BasicAuth) -> Value {
     json!([
         { "id": 1, "name": "John Doe" },
         { "id": 2, "name": "Jane Doe" }
