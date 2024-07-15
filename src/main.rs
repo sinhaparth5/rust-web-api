bee21b4dc6342a24075ed8719713ea76c511d34e
Parth Sinha <sinhaparth555@gmail.com>
2024-07-15 23:26:02 +0100

    Basic Auth function added

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,39 @@
 use rocket::response::status;
 use rocket::serde::json::{Value, json};
 
+struct BasicAuth {
+    pub username: String,
+    pub password: String,
+}
+
+ impl BasicAuth {
+     fn from_authorization_header(header: &str) -> Option<BasicAuth> {
+         let split = header.split_whitespace().collect::<Vec<_>>();
+         if split.len() != 2 {
+             return None;
+         }
+
+         if split[0] != "Basic" {
+             return None;
+         }
+         Self::from_base64_encoded(split[1])
+     }
+
+     fn from_base64_encoded(base64_string: &str) -> Option<BasicAuth> {
+         let decoded = base64::decode(base64_string).ok()?;
+         let decoded_str = String::from_utf8(decoded).ok()?;
+         let split = decoded_str.split(":").collect::<Vec<_>>();
+         if split.len() != 2 {
+             return None;
+         }
+         let (username, password) = (split[0].to_string(), split[1].to_string());
+         Some(BasicAuth {
+             username,
+             password
+         })
+     }
+ }
+
 #[get("/rust-api")]
 fn get_rust_api() -> Value {
     json!([
